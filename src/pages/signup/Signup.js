d34aca946a8e4cabd4818f4844e13e1e684e42b2
Ahmed-Abdella <abdellaa921@gmail.com>
2022-02-22 02:07:15 +0200

    create signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+        <label>
+          <span>email:</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          ></input>
+        </label>
+        <label>
+          <span>password:</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          ></input>
+        </label>
+
+        <label>
+          <span>Display name:</span>
+          <input
+            type="text"
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          ></input>
+        </label>
+        <button className="btn">Signup</button>
+      </form>
+    </div>
+  );
 }
